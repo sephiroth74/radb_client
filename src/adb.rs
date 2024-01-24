5acd51a70054864e030e7a80048a6f1a5af276d3
Alessandro Crugnola <alessandro.crugnola@swisscom.com>
2024-01-24 16:42:39 +0100

    doc

--- src/adb.rs
+++ src/adb.rs
@@ -18,11 +18,34 @@ use crate::result::Result;
 use crate::types::{Adb, AdbDevice, ConnectionType};
 
 impl Adb {
+	/// Create a new adb instance, or error if abd cannot be found in the user PATH.
+	///
+	/// # Examples:
+	/// ```rust
+	/// use radb_client::types::Adb;
+	/// let adb = Adb::new().expect("failed to find adb");
+	/// ```
 	pub fn new() -> Result<Adb> {
 		let adb = which("adb")?;
 		Ok(Adb(adb))
 	}
 
+	/// Execute a custom `adb` command with an optional cancel signal and timeout.
+	/// Use debug true to toggle tracing verbosity.
+	///
+	/// # Examples:
+	/// ```rust
+	/// use radb_client::types::{Adb, ConnectionType};
+	///
+	/// let adb = Adb::new().expect("failed to find adb");
+	/// let addr = ConnectionType::try_from("192.168.1.100:5555").unwrap();
+	///
+	/// // this will execute the command: adb -s 192.168.1.100:5555 get-state
+	/// match adb.exec(addr, vec!["get-state"], None, None, true) {
+	/// 	Ok(output) => println!("output: {output}"),
+	/// 	Err(err) => eprintln!("error: {err}"),
+	/// }
+	/// ```
 	pub fn exec<'a, C, I, S>(
 		&self,
 		addr: C,
@@ -46,6 +69,14 @@ impl Adb {
 	}
 
 	/// Check if mdns is available
+	/// # Examples
+	/// ```rust
+	/// 	use radb_client::types::Adb;
+	///
+	/// 	let adb = Adb::new().expect("failed to find adb");
+	///    	let mdns = adb.mdns_check(true);
+	///    	println!("mdns available: {mdns}");
+	/// ```
 	pub fn mdns_check(&self, debug: bool) -> bool {
 		CommandBuilder::adb(&self)
 			.with_debug(debug)
@@ -59,6 +90,28 @@ impl Adb {
 	}
 
 	/// List connected devices
+	///
+	/// # Examples:
+	/// ```rust
+	/// use lazy_static::lazy_static;
+	/// use regex::Regex;
+	/// use radb_client::types::{Adb, Client};
+	///
+	/// fn connect_emulator() -> Client {
+	///    	lazy_static! {
+	///    		static ref RE: Regex = Regex::new(r#"^emulator-*.+$"#).unwrap();
+	///    	}
+	///    	let devices = Adb::new().unwrap().list_devices(true).expect("failed to list devices");
+	///    	let device = devices
+	///    		.iter()
+	///    		.find(|device| {
+	///    			println!("Checking {device}...");
+	///    			RE.is_match(&device.name)
+	///    		}).expect("no emulator found");
+	///    		Client::try_from(device).expect("failed to create client from device")
+	/// }
+	///
+	/// ```
 	pub fn list_devices(&self, debug: bool) -> Result<Vec<AdbDevice>> {
 		let output = Cmd::builder(self.0.as_path())
 			.args([
@@ -112,6 +165,7 @@ impl Adb {
 		Ok(devices)
 	}
 
+	/// Disconnect from all connected devices
 	pub fn disconnect_all(&self, debug: bool) -> Result<bool> {
 		match Cmd::builder(self.0.as_path())
 			.with_debug(debug)
@@ -144,7 +198,7 @@ impl Adb {
 		Ok(output.success())
 	}
 
-	/// adb version
+	/// Return the adb version
 	pub fn version(&self, debug: bool) -> Result<String> {
 		lazy_static! {
 			static ref RE: Regex = Regex::new(r#"^Version\s+(?P<version>[\d+\.-]+)$"#).unwrap();
@@ -178,6 +232,12 @@ impl Adb {
 	}
 }
 
+impl From<PathBuf> for Adb {
+	fn from(value: PathBuf) -> Self {
+		Adb(value)
+	}
+}
+
 impl std::fmt::Display for Adb {
 	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
 		write!(f, "{:?}", self.0.to_str())
@@ -205,6 +265,7 @@ impl Into<PathBuf> for Adb {
 #[cfg(test)]
 pub(crate) mod test {
 	use std::path::PathBuf;
+	use which::which;
 
 	use crate::test::test::init_log;
 	use crate::types::{Adb, Client, ConnectionType};
@@ -214,6 +275,13 @@ pub(crate) mod test {
 	#[test]
 	fn test_adb() {
 		let _adb = Adb::new().expect("failed to find adb command in you PATH");
+	}
+
+	#[test]
+	fn test_adb_from() {
+		let path = which("adb").expect("failed to find adb");
+		let adb = Adb::from(path);
+		println!("adb: {}", adb);
 	}
 
 	#[test]
